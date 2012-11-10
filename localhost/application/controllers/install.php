f105db39e5c7df050cc0856c6fd8626291c9a48f
Tim Habersack <tim@hithlonde.com>
2012-11-10 13:46:52 -0800

    Have forge working with one table.

--- localhost/application/controllers/install.php
+++ localhost/application/controllers/install.php
@@ -9,6 +9,38 @@ class Install extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->database();
+		// Going to attempt to use forge to create a table!
+		$this->load->dbforge();
+		$fields = array(
+            'system_id' => array(
+                                'type' => 'INT',
+                                'constraint' => 8,
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE
+                            ),  
+            'system_name' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => 199
+                            ),                      
+			'system_public_uri' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => 299
+                            ), 
+			'system_xml' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => 90 //set to 'none' if using DB instead
+                            ),
+			'system_feed1' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => 299 
+                            )
+
+                );
+		$this->dbforge->add_field($fields);
+		$this->dbforge->add_key('system_id', TRUE);
+		$this->dbforge->create_table('systems',TRUE);
+
 		$this->load->view('install_complete');
 	}
 }
